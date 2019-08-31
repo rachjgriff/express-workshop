86814caa83b7ee7931bd1be91c3658d589d7a45d
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-08-31 12:09:17 +0100

    FEATURE: Create new routes with different endpoints

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ app.get("/", function (req, res) {
   res.send("Yay Node Girls!");
 });
 
+app.get("/node", function (req, res){
+  res.send("I'm learning node!");
+});
+
+app.get("/girls", function (req, res){
+  res.send("Girls are great!");
+});
+
 app.listen(3000, function() {
   console.log('Server is listening on port 3000. Ready to accept requests!');
 });
