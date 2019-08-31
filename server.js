7fea7808160aab23ff434851c9544baf9148540e
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-08-31 12:17:51 +0100

    FEATURE: Serve static files in public folder through express middleware function

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function (req, res) {
-  res.send("Yay Node Girls!");
-});
-
-app.get("/node", function (req, res){
-  res.send("I'm learning node!");
-});
-
-app.get("/girls", function (req, res){
-  res.send("Girls are great!");
-});
+app.use(express.static("public"));
 
 app.listen(3000, function() {
   console.log('Server is listening on port 3000. Ready to accept requests!');
