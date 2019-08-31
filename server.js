650ce4db5018458f93594f5bfbb9c7481fd24937
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-08-31 12:54:22 +0100

    FEATURE: Add blogpost text area with submit button & create POST route

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.use(express.static("public"));
 
+app.post("/create-post", function(req, res) {
+  console.log("/create-post");
+});
+
 app.listen(3000, function() {
   console.log('Server is listening on port 3000. Ready to accept requests!');
 });
